d1dbb18cb12d768aa98c6c77ec8fafee3f0c557b
Wes Seago <wseago2@gmail.com>
2015-02-20 22:48:41 -0500

    Initial commit

---
+++ app/src/main/java/com/wesleyseago/android/geoquiz/TrueFalse.java
@@ -0,0 +1,32 @@
+package com.wesleyseago.android.geoquiz;
+
+/**
+ * Created by Wesley on 2/20/2015.
+ */
+public class TrueFalse {
+    private int mQuestion;
+    private Boolean mTrueQuestion;
+
+    public TrueFalse(int question, boolean trueQuestion) {
+        mQuestion = question;
+        mTrueQuestion = trueQuestion;
+    }
+
+    public int getQuestion() {
+        return mQuestion
+    }
+
+    public void setQuestion(int question) {
+        mQuestion = question;
+    }
+
+    public boolean isTrueQuestion() {
+        return mTrueQuestion;
+    }
+
+    public void setTrueQuestion(boolean trueQuestion) {
+        mTrueQuestion = trueQuestion;
+    }
+
+
+}
